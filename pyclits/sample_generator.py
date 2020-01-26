2e2b821d9bf50cb9f04dab1f2176a8bcd2f99589
mafycek <h.lavicka@email.cz>
2020-01-26 23:39:08 +0100

    Sample generator from an array

--- pyclits/sample_generator.py
+++ pyclits/sample_generator.py
@@ -1,5 +1,23 @@
 import roessler_system
 
+def samples_from_arrays(data ,**kwargs):
+    if "history" in kwargs:
+        history = kwargs["history"]
+    else:
+        history = 5
+
+    sampled_dataset = []
+
+    shape_of_array = data.shape
+    for item in range(shape_of_array[1]-history):
+        sample = []
+        for hist in range(history):
+            for dim in range(shape_of_array[0]):
+                sample.append(data[dim, item + hist])
+        sampled_dataset.append(sample)
+
+    return sampled_dataset
+
 
 
 if __name__ == "__main__":
@@ -7,3 +25,6 @@ if __name__ == "__main__":
     kwargs["tStop"] = 50
     sol = roessler_system.roessler_oscillator(**kwargs)
     print(sol)
+
+    samples = samples_from_arrays(sol.y)
+    print(len(samples), sol.y.shape)
