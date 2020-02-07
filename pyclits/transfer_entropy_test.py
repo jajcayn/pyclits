3a02caae6ffe986a5fe949cc467653851736668e
mafycek <h.lavicka@email.cz>
2020-02-07 22:33:07 +0100

    Fix of transfer entropy

--- pyclits/transfer_entropy_test.py
+++ pyclits/transfer_entropy_test.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     sol = roessler_oscillator(**kwargs)
 
     joint_solution = sol.y
-    # samples_joint = samples_from_arrays(joint_solution)
+
     marginal_solution_1 = sol.y[0:3, :].T
     marginal_solution_2 = sol.y[3:6, :].T
 
@@ -20,5 +20,10 @@ if __name__ == "__main__":
     samples_marginal_1 = samples_from_arrays(marginal_solution_1, **kwargs)
     samples_marginal_2 = samples_from_arrays(marginal_solution_2, **kwargs)
 
-    transfer_entropy = renyi_transfer_entropy(marginal_solution_1, samples_marginal_1, samples_marginal_2,
-                                              **{"axis_to_join": 1})
+    results = {}
+    alphas = [0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999, 1.0, 1.001, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4,
+              1.5, 1.7, 1.8, 1.9]
+    for alpha in alphas:
+        transfer_entropy = renyi_transfer_entropy(marginal_solution_1, samples_marginal_1, samples_marginal_2,
+                                                  **{"axis_to_join": 1, "method": "LeonenkoProzanto", "alpha": alpha})
+        results[alpha] = transfer_entropy
