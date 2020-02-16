8dd2a47e7758684a5772efd0aa7af46448685baa
mafycek <h.lavicka@email.cz>
2020-02-17 00:11:25 +0100

    Processing of transfer entropy

--- pyclits/processing_transfer_entropy.py
+++ pyclits/processing_transfer_entropy.py
@@ -9,10 +9,20 @@ import pandas as pd
 if __name__ == "__main__":
     files = glob.glob("transfer_entropy/Transfer_entropy-*.bin")
     print(files)
+    frames = []
     for file in files:
         epsilon = float(file.split("-")[1].split(".b")[0])
         path = Path(file)
-        with open(path, "rb") as fb:
-            table = pd.read_pickle(fb)
 
-        print(table)
+        table = pd.read_pickle(path)
+
+        frame = pd.DataFrame(table)
+        frame["epsilon"] = epsilon
+        old_columns = frame.columns
+        frame = frame.reset_index()
+        frame.columns = ["alpha", f"transfer_entropy_{old_columns[0]}", "epsilon"]
+        frames.append(frame)
+
+    join_table = pd.concat(frames, ignore_index=True)
+    print(join_table)
+    print(pd.pivot_table(join_table, index=['alpha', 'epsilon']))
