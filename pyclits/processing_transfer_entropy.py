34101fa9100a9f18d67f22773bf5d68b424bd706
mafycek <h.lavicka@email.cz>
2020-02-22 21:44:07 +0100

    Processing of transfer entropy

--- pyclits/processing_transfer_entropy.py
+++ pyclits/processing_transfer_entropy.py
@@ -20,9 +20,17 @@ if __name__ == "__main__":
         frame["epsilon"] = epsilon
         old_columns = frame.columns
         frame = frame.reset_index()
-        frame.columns = ["alpha", f"transfer_entropy_{old_columns[0]}", "epsilon"]
+        # print(old_columns)
+        now_columns = [f"transfer_entropy_{item[1]}" for item in old_columns[:-1]]
+        column_names = ["alpha"]
+        column_names.extend(now_columns)
+        column_names.append("epsilon")
+        # .append(["epsilon"])
+        frame.columns = column_names
         frames.append(frame)
 
     join_table = pd.concat(frames, ignore_index=True)
     print(join_table)
-    print(pd.pivot_table(join_table, index=['alpha', 'epsilon']))
+    pivot_table = pd.pivot_table(join_table, index=['alpha', 'epsilon'])
+
+    pivot_table.to_pickle("transfer_entropy/pivot.bin")
