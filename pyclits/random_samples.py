a6846338684f9d82d9db82c3c702fc7dc3c1c717
mafycek <h.lavicka@email.cz>
2020-08-31 12:42:27 +0200

    Fix of examples

--- pyclits/random_samples.py
+++ pyclits/random_samples.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy.stats as stat
 
 
-def sample_normal_distribution(sigma, size_sample):
+def sample_normal_distribution(sigma, size_sample=10):
     if isinstance(sigma, np.ndarray) and len(sigma.shape) == 2 and (sigma.shape[0] == sigma.shape[1]):
         dimension = sigma.shape[0]
         uncorrelated_sample = np.random.normal(0, 1.0, (dimension, size_sample))
@@ -21,7 +21,7 @@ def sample_normal_distribution(sigma, size_sample):
         raise ArithmeticError("sigma parameter has wrong type")
 
 
-def sample_elliptical_contour_stable(sigma, alpha, gamma=1.0, delta=np.array([0, 0]), size_sample=10):
+def sample_elliptical_contour_stable(sigma, alpha, gamma=1.0, delta=np.array([0]), size_sample=10):
     recalculated_gamma = 2 * np.power(gamma, 2) * np.power(np.cos(np.pi * alpha / 4), (2 / alpha))
     random_stable = stat.levy_stable.rvs(alpha=alpha / 2.0, beta=1.0, loc=0, scale=recalculated_gamma, size=size_sample)
     sqrt_random_stable = np.sqrt(random_stable)
@@ -48,7 +48,7 @@ def sample_linear_depended_stable(sigma, alpha, beta=np.array([0]), delta=np.arr
         raise Exception("Invalid input detected")
 
 
-def sample_asymmetric_laplace_distribution(sigma, mean, size_sample):
+def sample_asymmetric_laplace_distribution(sigma=np.array([1]), mean=np.array([0]), size_sample=10):
     exponentially_distributed = stat.expon.rvs(size=size_sample)
     sqrt_exponentially_distributed = np.sqrt(exponentially_distributed)
     random_normal = sample_normal_distribution(sigma, size_sample)
@@ -59,7 +59,7 @@ def sample_asymmetric_laplace_distribution(sigma, mean, size_sample):
     return stable_samples
 
 
-def sample_student_t_distribution(sigma, mean, degrees_of_freedom, size_sample):
+def sample_student_t_distribution(degrees_of_freedom, sigma=np.array([1]), mean=np.array([0]), size_sample=10):
     chi_squared_distributed = stat.chi2.rvs(df=degrees_of_freedom, size=size_sample)
     random_normal = sample_normal_distribution(sigma, size_sample)
 
@@ -70,29 +70,30 @@ def sample_student_t_distribution(sigma, mean, degrees_of_freedom, size_sample):
 
 
 if __name__ == "__main__":
-    sigma = np.array([[1, 0.4], [0.4, 1]])
+    sigma = np.array([[1, -0.2], [-0.2, 1]])
+    samples = 10000
 
-    normal_samples = sample_normal_distribution(sigma, 10000)
+    normal_samples = sample_normal_distribution(sigma, size_sample=samples)
 
     plt.scatter(normal_samples[:, 0], normal_samples[:, 1], marker=".")
     plt.show()
 
-    stable_linear_depended_samples = sample_linear_depended_stable(sigma, alpha=1.9, beta=np.array([0, 0.9]), delta=np.array([0, 0]), size_sample=10000)
+    stable_linear_depended_samples = sample_linear_depended_stable(sigma, alpha=1.9, beta=np.array([-0.3, 0.95]), delta=np.array([1, 0]), size_sample=samples)
 
     plt.scatter(stable_linear_depended_samples[:, 0], stable_linear_depended_samples[:, 1], marker=".")
     plt.show()
 
-    samples_stable = sample_elliptical_contour_stable(sigma, 1.6, 1.0, np.array([0, 0]), 10000)
+    samples_stable = sample_elliptical_contour_stable(sigma, 1.6, 1.0, np.array([0, 0]), samples)
 
     plt.scatter(samples_stable[:, 0], samples_stable[:, 1], marker=".")
     plt.show()
 
-    samples_laplace = sample_asymmetric_laplace_distribution(sigma, np.array([0, 0]), 10000)
+    samples_laplace = sample_asymmetric_laplace_distribution(sigma, np.array([0, 0]), samples)
 
     plt.scatter(samples_laplace[:, 0], samples_laplace[:, 1], marker=".")
     plt.show()
 
-    samples_student = sample_student_t_distribution(sigma, np.array([0, 0]), 2, 10000)
+    samples_student = sample_student_t_distribution(sigma, np.array([0, 0]), 2, samples)
 
     plt.scatter(samples_student[:, 0], samples_student[:, 1], marker=".")
     plt.show()
