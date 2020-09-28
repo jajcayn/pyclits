b5dac6cd9e11e359347ded182604d067deeaa737
mafycek <h.lavicka@email.cz>
2020-09-28 16:12:10 +0200

    Renyi entropy of Laplace distribution

--- pyclits/random_samples.py
+++ pyclits/random_samples.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.special as spec
 import scipy.stats as stat
+from numpy.linalg import inv
+
+
+def sample_laplace_distribution(sigma, q, n, m):
+    i_sigma = inv(sigma)
+
+    def h(sigma, m, i_sigma):
+        i_sigma = inv(sigma)
+
+        arg1 = np.dot(m, i_sigma)
+        arg2 = np.dot(arg1, m)
+        arg3 = np.dot(np.dot(arg1, sigma), arg1)
+
+        return arg2 - arg3
+
+    h_arg = h(sigma, m, i_sigma)
+
+    arg1 = (pow(2 * np.pi, n) * 0.5 / gamma(q)) * pow(q, -n) * np.exp(0.5 / h_arg)
+
+    loop = int(q - 0.5 * n)
+
+    arg2 = 0
+    for l in range(loop):
+        arg21 = pow(2 / h_arg, 0.5 * (q - 0.5 * n - l))
+        arg22 = pow(-1 / h_arg, l)
+        arg23 = gamma(q - 0.5 * n) * gamma(0.5 * (q - 0.5 * n - l)) / (gamma(l + 1) * gamma(q - 0.5 * n - l))
+        arg2 += arg21 * arg22 * arg23
+
+    return arg1 * arg2
 
 
 def sample_normal_distribution(sigma, size_sample=10):
@@ -31,6 +63,10 @@ def sample_elliptical_contour_stable(sigma, alpha, gamma=1.0, delta=np.array([0]
     stable_samples = np.array([multiplicator * vector - delta for multiplicator, vector in zip(sqrt_random_stable, random_normal)])
 
     return stable_samples
+
+
+def sample_sub_linear_levy_stable(sigma, alpha, alpha_orig, gamma=1.0, delta=np.array([0]), size_sample=10):
+    pass
 
 
 def sample_linear_depended_stable(sigma, alpha, beta=np.array([0]), delta=np.array([0]), size_sample=10):
@@ -70,35 +106,87 @@ def sample_student_t_distribution(degrees_of_freedom, sigma=np.array([1]), mean=
     return stable_samples
 
 
+def Renyi_student_t_distribution(degrees_of_freedom: float, sigma, q: float, determinant=None):
+    dimension = sigma.shape[0]
+    if determinant:
+        arg = pow(determinant, 0.5 * (1. - q))
+    else:
+        arg = pow(np.linalg.det(sigma), 0.5 * (1. - q))
+
+    arg2 = pow(degrees_of_freedom * np.pi, 0.5 * dimension * (1. - q))
+    arg31 = pow(spec.gamma((degrees_of_freedom + dimension) * 0.5), q)
+    arg32 = spec.gamma(q * (degrees_of_freedom + dimension) * 0.5 - p * 0.5)
+    arg33 = spec.gamma(q * (degrees_of_freedom + dimension) * 0.5)
+    arg34 = pow(spec.gamma(degrees_of_freedom * 0.5), q)
+    arg3 = (arg31 * arg32) / (arg33 * arg34)
+
+    return 1. / (1. - q) * np.log2(arg3 * arg * arg2)
+
+
+def Renyi_normal_distribution(sigma, alpha):
+    if isinstance(sigma, float):
+        return Renyi_normal_distribution_1D(sigma, alpha)
+    elif isinstance(sigma, (np.matrix)):
+        return Renyi_normal_distribution_ND(sigma, alpha)
+    else:
+        raise ArithmeticError("sigma parameter has wrong type")
+
+
+def Renyi_normal_distribution_1D(sigma_number, alpha):
+    if alpha == 1:
+        return math.log(2 * math.pi * math.exp(1) * np.power(sigma_number, 2)) / 2
+    else:
+        return math.log(2 * math.pi) / 2 + math.log(sigma_number) + math.log(alpha) / (alpha - 1) / 2
+
+
+def Renyi_normal_distribution_ND(sigma_matrix: np.matrix, alpha, determinant=None):
+    dimension = sigma_matrix.shape[0]
+    if alpha == 1:
+        return math.log(2 * math.pi * math.exp(1)) * dimension / 2.0 + np.log(np.sqrt(np.linalg.det(sigma_matrix)))
+    else:
+        if determinant:
+            return math.log(2 * math.pi) * dimension / 2.0 + math.log(determinant) / 2.0 + dimension * math.log(alpha) / (alpha - 1) / 2
+        else:
+            return math.log(2 * math.pi) * dimension / 2.0 + math.log(np.linalg.det(sigma_matrix)) / 2.0 + dimension * math.log(alpha) / (alpha - 1) / 2
+
+
+def Renyi_student_t_distribution_1D(sigma, degrees_of_freedom, alpha):
+    if isinstance(sigma, float) or isinstance(sigma, int):
+        dimension = 1
+        determinant = sigma
+    elif isinstance(sigma, np.matrix):
+        if len(sigma.shape) == 2 and (sigma.shape[0] == sigma.shape[1]):
+            dimension = sigma.shape[0]
+            determinant = np.linalg.det(sigma)
+        else:
+            raise ArithmeticError("sigma parameter has wrong type")
+    else:
+        raise ArithmeticError("sigma parameter has wrong type")
+
+    if alpha == 1:
+        return (degrees_of_freedom + 1.0) / 2 * (spec.digamma((degrees_of_freedom + 1.0) / 2) - spec.digamma((degrees_of_freedom) / 2)) + np.log2(
+            np.sqrt(degrees_of_freedom) * spec.beta(0.5, degrees_of_freedom / 2.0))
+    else:
+        nominator = spec.beta(dimension / 2.0, alpha * (dimension + degrees_of_freedom) / 2.0 - dimension / 2.0)
+        denominator = math.pow(spec.beta(degrees_of_freedom / 2.0, dimension / 2.0), alpha)
+        beta_factor = math.log2(nominator / denominator)
+        return 1 / (1 - alpha) * beta_factor * math.log2(math.pow(np.pi * degrees_of_freedom, dimension) * determinant) - math.log2(spec.gamma(dimension / 2.0))
+
+
+def Renyi_beta_distribution(a, b, alpha):
+    return 1 / (1 - alpha) * math.log2(spec.beta(alpha * a + alpha - 1, alpha * b + alpha - 1) / math.pow(spec.beta(a, b), alpha))
+
+
 if __name__ == "__main__":
     sigma = np.array([[1, -0.1], [-0.1, 1]])
-    samples = 10000000
+    samples = 1000000
 
     normal_samples = sample_normal_distribution(sigma=sigma, size_sample=samples)
-
-    # plt.scatter(normal_samples[:, 0], normal_samples[:, 1], marker=".")
-    # plt.show()
-
-    stable_linear_depended_samples = sample_linear_depended_stable(sigma=sigma, alpha=1.99, beta=np.array([0.01, 0.01]), delta=np.array([0, 0]),
+    stable_linear_depended_samples = sample_linear_depended_stable(sigma=sigma, alpha=1.999, beta=np.array([0.5, -0.5]), delta=np.array([0, 0]),
                                                                    size_sample=samples)
-
-    # plt.scatter(stable_linear_depended_samples[:, 0], stable_linear_depended_samples[:, 1], marker=".")
-    # plt.show()
-
     samples_stable = sample_elliptical_contour_stable(sigma, 1.95, 1.0, np.array([0, 0]), size_sample=samples)
-
-    # plt.scatter(samples_stable[:, 0], samples_stable[:, 1], marker=".")
-    # plt.show()
-
     samples_laplace = sample_asymmetric_laplace_distribution(sigma=sigma, mean=np.array([1, -1]), size_sample=samples)
-
-    # plt.scatter(samples_laplace[:, 0], samples_laplace[:, 1], marker=".")
-    # plt.show()
-
     samples_student = sample_student_t_distribution(sigma=sigma, mean=np.array([0, 0]), degrees_of_freedom=2, size_sample=samples)
-
-    # plt.scatter(samples_student[:, 0], samples_student[:, 1], marker=".")
-    # plt.show()
 
     datasamples = [[normal_samples, "Normal distribution"], [stable_linear_depended_samples, "Stable linear dependent"],
                    [samples_stable, "Stable sub-Gaussian"],
