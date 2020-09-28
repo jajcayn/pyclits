4d2d47e5b5b79a8184b24f3ac7c8bd08c2c64570
mafycek <h.lavicka@email.cz>
2020-09-28 16:13:18 +0200

    Refactoring of the Renyi entropy simulation

--- pyclits/entropy_calculation_test.py
+++ pyclits/entropy_calculation_test.py
@@ -2,11 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import math
 import time
-
-import numpy as np
-from scipy.special import gamma
 
 import mutual_inf
 from random_samples import *
@@ -14,82 +10,14 @@ from random_samples import *
 time_start = time.process_time()
 
 
-def Renyi_tstudent(sigma, q: float, p: float, nu: float, determinant=None):
-    if determinant:
-        arg = pow(determinant, 0.5 * (1. - q))
-    else:
-        arg = pow(np.linalg.det(sigma), 0.5 * (1. - q))
-
-    arg2 = pow(nu * np.pi, 0.5 * p(1. - q))
-    arg31 = pow(gamma((nu + p) * 0.5), q)
-    arg32 = gamma(q * (nu + p) * 0.5 - p * 0.5)
-    arg33 = gamma(q * (nu + p) * 0.5)
-    arg34 = pow(gamma(nu * 0.5), q)
-    arg3 = (arg31 * arg32) / (arg33 * arg34)
-
-    return 1. / (1. - q) * np.log2(arg3 * arg * arg2)
-
-
-def Renyi_normal_distribution(sigma, alpha):
-    if isinstance(sigma, float):
-        return Renyi_normal_distribution_1D(sigma, alpha)
-    elif isinstance(sigma, (np.matrix)):
-        return Renyi_normal_distribution_ND(sigma, alpha)
-    else:
-        raise ArithmeticError("sigma parameter has wrong type")
-
-
-def Renyi_normal_distribution_1D(sigma_number, alpha):
-    if alpha == 1:
-        return math.log(2 * math.pi * math.exp(1) * np.power(sigma_number, 2)) / 2
-    else:
-        return math.log(2 * math.pi) / 2 + math.log(sigma_number) + math.log(alpha) / (alpha - 1) / 2
-
-
-def Renyi_normal_distribution_ND(sigma_matrix: np.matrix, alpha, determinant=None):
-    dimension = sigma_matrix.shape[0]
-    if alpha == 1:
-        return math.log(2 * math.pi * math.exp(1)) * dimension / 2.0 + np.log(np.sqrt(np.linalg.det(sigma_matrix)))
-    else:
-        if determinant:
-            return math.log(2 * math.pi) * dimension / 2.0 + math.log(determinant) / 2.0 + dimension * math.log(alpha) / (alpha - 1) / 2
-        else:
-            return math.log(2 * math.pi) * dimension / 2.0 + math.log(np.linalg.det(sigma_matrix)) / 2.0 + dimension * math.log(alpha) / (alpha - 1) / 2
-
-
-def Renyi_student_t_distribution_1D(sigma, degrees_of_freedom, alpha):
-    if isinstance(sigma, float) or isinstance(sigma, int):
-        dimension = 1
-        determinant = sigma
-    elif isinstance(sigma, np.matrix):
-        if len(sigma.shape) == 2 and (sigma.shape[0] == sigma.shape[1]):
-            dimension = sigma.shape[0]
-            determinant = np.linalg.det(sigma)
-        else:
-            raise ArithmeticError("sigma parameter has wrong type")
-    else:
-        raise ArithmeticError("sigma parameter has wrong type")
-
-    if alpha == 1:
-        return (degrees_of_freedom+1.0)/2*(spec.digamma((degrees_of_freedom+1.0)/2) - spec.digamma((degrees_of_freedom)/2))+np.log2(np.sqrt(degrees_of_freedom) * spec.beta(0.5, degrees_of_freedom/2.0))
-    else:
-        nominator = spec.beta(dimension/2.0, alpha*(dimension+degrees_of_freedom)/2.0 - dimension/2.0)
-        denominator = math.pow(spec.beta(degrees_of_freedom / 2.0, dimension / 2.0), alpha)
-        beta_factor = math.log2(nominator / denominator)
-        return 1 / (1 - alpha) * beta_factor * math.log2(math.pow(np.pi*degrees_of_freedom, dimension)*determinant) - math.log2(spec.gamma(dimension/2.0))
-
-
-def Renyi_beta_distribution(a, b, alpha):
-    return 1 / (1 - alpha) * math.log2(spec.beta(alpha*a+alpha-1, alpha*b+alpha-1) / math.pow(spec.beta(a, b), alpha))
-
-
-def complete_test_1D(filename="statistics.txt", samples = 1000,
-                  alphas=[0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9],
-                  mu = 0, sigmas = [0.1, 0.5, 1.0, 5.0, 10.0, 50, 100],
-                  sizes_of_sample = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000],
-                  theoretical_value_function=lambda sigma, alpha: Renyi_normal_distribution_1D(sigma, alpha),
-                  sample_generator=lambda mu, sigma, size_sample: np.random.normal(mu, sigma, (size_sample, 1)),
-                  sample_estimator=lambda data_samples, alpha: mutual_inf.renyi_entropy(data_samples, method="LeonenkoProzanto", indices_to_use=[1, 2, 3, 4, 5], alpha=alpha)):
+def complete_test_1D(filename="statistics.txt", samples=1000,
+                     alphas=[0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9],
+                     mu=0, sigmas=[0.1, 0.5, 1.0, 5.0, 10.0, 50, 100],
+                     sizes_of_sample=[10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000],
+                     theoretical_value_function=lambda sigma, alpha: Renyi_normal_distribution_1D(sigma, alpha),
+                     sample_generator=lambda mu, sigma, size_sample: np.random.normal(mu, sigma, (size_sample, 1)),
+                     sample_estimator=lambda data_samples, alpha: mutual_inf.renyi_entropy(data_samples, method="LeonenkoProzanto",
+                                                                                           indices_to_use=[1, 2, 3, 4, 5], alpha=alpha)):
     with open(filename, "wt") as fd:
         entropy_samples = {}
         duration_samples = {}
@@ -122,25 +50,31 @@ def complete_test_1D(filename="statistics.txt", samples = 1000,
                         duration_samples[sample_position].append(duration)
                         difference_samples[sample_position].append(difference)
 
-                        #print(f"samples={size_sample}, duration={time_end-time_start}, alpha={alpha}, tested_estimator={entropy}, theoretical_calculation={theoretical_value}, difference={difference}")
+                        # print(f"samples={size_sample}, duration={time_end-time_start}, alpha={alpha}, tested_estimator={entropy}, theoretical_calculation={theoretical_value}, difference={difference}")
 
-                    print(f"{alpha} {size_sample} {sigma} {np.mean(entropy_samples[sample_position])} {np.std(entropy_samples[sample_position])} {np.mean(duration_samples[sample_position])} {np.std(duration_samples[sample_position])} {np.mean(difference_samples[sample_position])} {np.std(difference_samples[sample_position])}", file=fd)
+                    print(
+                        f"{alpha} {size_sample} {sigma} {np.mean(entropy_samples[sample_position])} {np.std(entropy_samples[sample_position])} {np.mean(duration_samples[sample_position])} {np.std(duration_samples[sample_position])} {np.mean(difference_samples[sample_position])} {np.std(difference_samples[sample_position])}",
+                        file=fd)
 
 
-def complete_test_ND(filename="statistics.txt", samples = 1000, sigma_skeleton = np.identity(10),
-                  alphas=[0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9],
-                  mu = 0, sigmas = [0.1, 0.5, 1.0, 5.0, 10.0, 50, 100],
-                  sizes_of_sample = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000], indices_to_use=[1, 2, 3, 4, 5, 6, 7, 8, 9],
-                  theoretical_value_function=lambda sigma, alpha: Renyi_normal_distribution_ND(sigma, alpha),
-                  sample_generator=lambda mu, sigma, size_sample: sample_normal_distribution(sigma, size_sample),
-                  sample_estimator=lambda data_samples, alpha, indices_to_use: mutual_inf.renyi_entropy(data_samples, method="LeonenkoProzanto", indices_to_use=indices_to_use, alpha=alpha)):
+def complete_test_ND(filename="statistics.txt", samples=1000, sigma_skeleton=np.identity(10),
+                     alphas=[0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9],
+                     mu=0, sigmas=[0.1, 0.5, 1.0, 5.0, 10.0, 50, 100],
+                     sizes_of_sample=[10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000], indices_to_use=[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                     theoretical_value_function=lambda sigma, alpha: Renyi_normal_distribution_ND(sigma, alpha),
+                     sample_generator=lambda mu, sigma, size_sample: sample_normal_distribution(sigma, size_sample),
+                     sample_estimator=lambda data_samples, alpha, indices_to_use: mutual_inf.renyi_entropy(data_samples, method="LeonenkoProzanto",
+                                                                                                           indices_to_use=indices_to_use, alpha=alpha)):
     with open(filename, "wt") as fd:
         real_indeces = []
         print("alpha\tsample size\tsigma\ttheoretical value\t", file=fd, end="")
         for index in indices_to_use:
-            print(f"mean Renyi entropy {index}\tstd Renyi entropy {index}\tmean computer time {index}\tstd computer time {index}\tmean difference {index}\tstd of difference {index}\t3rd moment of difference {index}\t", file=fd, end="")
+            print(
+                f"mean Renyi entropy {index}\tstd Renyi entropy {index}\tmean computer time {index}\tstd computer time {index}\tmean difference {index}\tstd of difference {index}\t3rd moment of difference {index}\t",
+                file=fd, end="")
             real_indeces.append([index])
-        print("mean Renyi entropy\tstd Renyi entropy\tmean computer time\tstd computer time\tmean difference\tstd of difference\t3rd moment of difference", file=fd)
+        print("mean Renyi entropy\tstd Renyi entropy\tmean computer time\tstd computer time\tmean difference\tstd of difference\t3rd moment of difference",
+              file=fd)
         real_indeces.append(indices_to_use)
 
         # collections of results
@@ -183,17 +117,17 @@ def complete_test_ND(filename="statistics.txt", samples = 1000, sigma_skeleton =
 
                         # save data for samples
                         print(
-                        f"{np.mean(entropy_samples[sample_position])}\t{np.std(entropy_samples[sample_position])}\t{np.mean(duration_samples[sample_position])}\t{np.std(duration_samples[sample_position])}\t{np.mean(difference_samples[sample_position])}\t{np.std(difference_samples[sample_position])}\t{stat.moment(difference_samples[sample_position], moment=3)}\t",
-                        file=fd, end="")
+                            f"{np.mean(entropy_samples[sample_position])}\t{np.std(entropy_samples[sample_position])}\t{np.mean(duration_samples[sample_position])}\t{np.std(duration_samples[sample_position])}\t{np.mean(difference_samples[sample_position])}\t{np.std(difference_samples[sample_position])}\t{stat.moment(difference_samples[sample_position], moment=3)}\t",
+                            file=fd, end="")
                     print("", file=fd, flush=True)
 
 
 def small_test():
     sample_array = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9]], dtype=float)
     input_sample = np.ndarray(shape=sample_array.shape, buffer=sample_array)
-    #print(input_sample)
-    #print(mutual_inf.renyi_entropy(np.matrix([[1],[2],[3],[4],[5],[6],[7],[8],[9]]), method="LeonenkoProzanto"))
-    #print(mutual_inf.renyi_entropy(input_sample, method="Paly"))
+    # print(input_sample)
+    # print(mutual_inf.renyi_entropy(np.matrix([[1],[2],[3],[4],[5],[6],[7],[8],[9]]), method="LeonenkoProzanto"))
+    # print(mutual_inf.renyi_entropy(input_sample, method="Paly"))
 
     mu = 0
     sigma = 10
@@ -206,16 +140,17 @@ def small_test():
             entropy = 0
             data_samples = np.random.normal(mu, sigma, (number_samples, 1))
             time_start = time.process_time()
-            #entropy = mutual_inf.renyi_entropy(samples, method="Lavicka", indices_to_use=[1])
+            # entropy = mutual_inf.renyi_entropy(samples, method="Lavicka", indices_to_use=[1])
             time_end = time.process_time()
-            #print(number_samples, time_end-time_start, entropy, Renyi_normal_distribution_1D(sigma, alpha))
+            # print(number_samples, time_end-time_start, entropy, Renyi_normal_distribution_1D(sigma, alpha))
 
             time_start = time.process_time()
             entropy = mutual_inf.renyi_entropy(data_samples, method="LeonenkoProzanto", indices_to_use=[1, 2, 3, 4, 5], alpha=alpha)
             theoretical_value = Renyi_normal_distribution_1D(sigma, alpha)
-            difference = abs(theoretical_value-entropy)
+            difference = abs(theoretical_value - entropy)
             time_end = time.process_time()
-            print(f"samples={number_samples}, duration={time_end-time_start}, alpha={alpha}, tested_estimator={entropy}, theoretical_calculation={theoretical_value}, difference={difference}")
+            print(
+                f"samples={number_samples}, duration={time_end - time_start}, alpha={alpha}, tested_estimator={entropy}, theoretical_calculation={theoretical_value}, difference={difference}")
 
 
 if __name__ == "__main__":
@@ -228,6 +163,10 @@ if __name__ == "__main__":
     parser.add_argument('--sigmas', metavar='XXX', type=float, nargs='+', help='Sigmas')
     parser.add_argument('--correlation', metavar='XXX', type=float, default=0.0, help='Sample sizes')
     parser.add_argument('--maximal_index', metavar='XXX', type=int, default=3, help='Maximal index')
+    parser.add_argument('--noise_type', metavar='XXX', type=str, default="gaussian", help='Type of noise that is to be investigated')
+    parser.add_argument('--arbitrary_precision', metavar='XXX', type=bool, default=False, help='Use of the arbitrary precision')
+    parser.add_argument('--arbitrary_precision_decimal_numbers', metavar='XXX', type=int, default="30", help='How many decimal numbers are used')
+    parser.add_argument('--metric', metavar='XXX', type=str, default="euclidean", help='Metric')
 
     args = parser.parse_args()
 
@@ -259,6 +198,24 @@ if __name__ == "__main__":
 
         alphas = [0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0, 1.01, 1.05, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7, 1.8, 1.9]
 
+    arbitrary_precision = args.arbitrary_precision
+    arbitrary_precision_decimal_numbers = args.arbitrary_precision_decimal_numbers
+    use_metric = args.metric
+    degrees_of_freedom = 3
+    alpha = 2.0
+
+    job_dictionary = {"gaussian": {"generator": lambda mu, sigma, size_sample: sample_normal_distribution(sigma, size_sample),
+                                   "theory": lambda sigma, alpha: Renyi_normal_distribution_ND(sigma, alpha)},
+                      "student": {"generator": lambda mu, sigma, size_sample: sample_student_t_distribution(degrees_of_freedom, sigma, mu, size_sample),
+                                  "theory": lambda sigma, alpha: Renyi_student_t_distribution(degrees_of_freedom, sigma, alpha)},
+                      "sub_gaussian": {"generator": lambda mu, sigma, size_sample: sample_elliptical_contour_stable(sigma, alpha, mu, size_sample),
+                                       "theory": None}}
+    estimator_dictionary = {"Renyi": lambda data_samples, alpha, indices_to_use: mutual_inf.renyi_entropy(data_samples, method=method,
+                                                                                                          indices_to_use=indices_to_use, alphas=alpha,
+                                                                                                          **{"arbitrary_precision": arbitrary_precision,
+                                                                                                             "arbitrary_precision_decimal_numbers": arbitrary_precision_decimal_numbers,
+                                                                                                             "metric": use_metric})}
+
     print(f"Calculation for dimensions {dimensions}")
 
     for dimension in dimensions:
@@ -267,8 +224,4 @@ if __name__ == "__main__":
                          sigma_skeleton=sigma_skeleton, sizes_of_sample=samples_sizes, indices_to_use=indices,
                          theoretical_value_function=lambda sigma, alpha: Renyi_normal_distribution_ND(sigma, alpha),
                          sample_generator=lambda mu, sigma, size_sample: sample_normal_distribution(sigma, size_sample),
-                         sample_estimator=lambda data_samples, alpha, indices_to_use: mutual_inf.renyi_entropy(data_samples, method=method,
-                                                                                                               indices_to_use=indices_to_use, alphas=alpha,
-                                                                                                               **{"arbitrary_precision": True,
-                                                                                                                  "arbitrary_precision_decimal_numbers": 50,
-                                                                                                                  "metric": "euclidean"}))
+                         sample_estimator=estimator_dictionary["Renyi"])
