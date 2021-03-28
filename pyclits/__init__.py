5bdc8cc93b246165400bd976a468efd238d949c4
mafycek <h.lavicka@email.cz>
2021-03-28 17:57:56 +0200

    Fix of imports from pyclits and fixes to python 2 prints

--- pyclits/__init__.py
+++ pyclits/__init__.py
@@ -4,6 +4,16 @@ created on Sep 22, 2017
 @author: Nikola Jajcay, jajcay(at)cs.cas.cz
 """
 
+from .data_loaders import *
+from .empirical_model import *
+from .functions import *
+from .geofield import *
+from .mutual_inf import *
+from .ssa import *
+from .surrogates import *
+from .var_model import *
+from .wavelet_analysis import *
+
 __all__ = ['data_loaders', 'empirical_model', 'functions', 'geofield', 'mutual_inf', 'ssa', 'surrogates',
            'var_model', 'wavelet_analysis']
 __author__ = "Nikola Jajcay <jajcay@cs.cas.cz>"
